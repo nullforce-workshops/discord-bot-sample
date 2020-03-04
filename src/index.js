2ebfe0ceb102ee7213dc8b3275805401a223ac74
nullforce <glenngit@nullforce.com>
2020-03-03 22:57:45 -0600

    Adding additional commands

--- src/index.js
+++ src/index.js
@@ -13,9 +13,17 @@ client.on("message", message => {
     console.log(message.content);
 
     if (message.content === getPrefixedCommand("ping")) {
+      logCommandReceived("ping");
       message.channel.send("pong!");
     } else if (message.content === getPrefixedCommand("boop")) {
+      logCommandReceived("boop");
       message.channel.send("boop!");
+    } else if (message.content === getPrefixedCommand("server")) {
+      logCommandReceived("server");
+      message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    } else if (message.content === getPrefixedCommand("user-info")) {
+      logCommandReceived("user-info");
+      message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     }
 });
 
@@ -23,4 +31,8 @@ client.login(discordBotToken);
 
 function getPrefixedCommand(command) {
   return `${prefix}${command}`;
+}
+
+function logCommandReceived(commandName) {
+  console.log(`command received: ${prefix}${commandName}`);
 }
